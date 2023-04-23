fe01e915edc9d1f5f24ba182c6820aa61615f29e
Fabiano Luiz Santos <fab_luiz@msn.com>
2023-04-23 12:21:35 -0300

    commit

--- script.js
+++ script.js
@@ -3,9 +3,23 @@ const form = document.querySelector('form')
 const inputWeight = document.querySelector('#weight')
 const inputHeight = document.querySelector("#height")
 
+const modalWrapper = document.querySelector('.modal-wrapper')
+const modalMessage = document.querySelector('.modal .tltle span')
+const modalBtnClose = document.querySelector('.modal button.clse')
+
+
 form.onsubmit = function(event) {
   event.preventDefault()
-
-  const wight = inputWeight.value
+  
+  const weight = inputWeight.value
   const height = inputHeight.value
+  const result = IMC(weight, height)
+  const message = `seu IMC é de ${result}`
+  
+  modalMessage.innerText = message
+  modalWrapper.classList.add('open')
+}
+
+function IMC(weight, height) {
+  return (weight / ((height / 100) ** 2)).toFixed(2)
 }
